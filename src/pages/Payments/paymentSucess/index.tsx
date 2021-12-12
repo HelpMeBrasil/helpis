e50597f121be941a8f74558cfda4bfd407ac543b
Gabriel Vianna Garcia <gabriel.garcia@safe2pay.com.br>
2021-12-12 09:47:39 -0300

    adjust in paymentsuccess page

--- src/pages/Payments/paymentSucess/index.tsx
+++ src/pages/Payments/paymentSucess/index.tsx
@@ -2,55 +2,94 @@ import { useLocation } from "react-router-dom";
 import { ContentLarge } from "../../../components/contentLarge";
 import { Label, Title } from "../../../components/form";
 import './styles.scss';
+import { toast } from 'react-toastify';
 
 
 export function PaymentSucess() {
   const {state} = useLocation();
-  const { description, walletAddress, barcode, amountBTC, qrCode, key, authenticationUrl } = state; // Read values passed on state
+  const { description, walletAddress, digitableLine, bankSlipUrl, amountBTC, qrCode, key, authenticationUrl } = state; // Read values passed on state
+
+  function handleCopyURL(codePaymentMethod: number){
+    if(codePaymentMethod == 1){
+      navigator.clipboard.writeText(digitableLine)
+      toast.success("Código de barras copiado!");
+    }
+    if(codePaymentMethod == 6){
+      navigator.clipboard.writeText(key)
+      toast.success("Copia e cola copiado!");
+    }
+    if(codePaymentMethod == 3){
+      navigator.clipboard.writeText(walletAddress)
+      toast.success("Endereço copiado!");
+    }
+  }
+
   return(
     <>
-    <ContentLarge>
-        <Title tag="h1" onClassName="title_h1" value="Informações abaixo sobre o pagamento:"/>
-        <hr></hr>
-    </ContentLarge>
-    <ContentLarge>
-        <Label center="center" valueName={description}/>
+
+      <div className="head">
+        <div className="dummy-positioning d-flex">
+          <div className="success-icon">
+            <div className="success-icon__tip"></div>
+            <div className="success-icon__long"></div>
+          </div>
+        </div>
+
+        <div className="description">
+          <Label valueName={description}/>
+        </div>
+        </div>
 
         {authenticationUrl === undefined ? '' :
         <>
-        <Label center="center" valueName="Url para concluir o pagamento"></Label>
-        <Label valueName={authenticationUrl}></Label>
+        <div className="debit">
+          <p>Para concluir o pagamento você precisa autenticar-se no seu ambiente bancário:</p>
+          <a className="url" href={authenticationUrl} target="_blank">{authenticationUrl}</a>
+        </div>
         </> }
 
-
-        {barcode === undefined ? '' : 
+        {digitableLine === undefined ? '' : 
         <>
-        <Label center="center" valueName="Codigo de barras"></Label>
-        <Label valueName={barcode}></Label>
+        <div className="digitableLine">
+        <p>Código de barras</p>
+        <button onClick={() => handleCopyURL(1)} className="copydigitableline">
+        <p title='Copiar'> {digitableLine} </p>
+        </button>
+        </div>
+        <div className="urlBankslip">
+          <p>Link do boleto</p>
+          <a href={bankSlipUrl} target="_blank">{bankSlipUrl}</a>
+        </div>
         </>
          }
 
         {key === undefined ? '' : 
         <>
-        <Label center="center" valueName="Key"></Label>
-        <Label valueName={key}></Label>
-        <Label center="center" valueName="QrCode"></Label>
-        <img alt="qrcode" className="imgQrCodePayment" src={qrCode}/>
+        <div>
+          <img alt="qrcode" className="imgQrCodePayment" src={qrCode}/>
+          <p className="copy">Chave copia e cola</p>
+          <button onClick={() => handleCopyURL(6)} className="copykey">
+          <p title='Copiar'> {key} </p>
+          </button>
+        </div>
         </>
          }
-         
-
         
         {walletAddress === undefined ? '' : 
         <>
-        <Label center="center" valueName="Endereço da carteira"></Label>
-        <Label valueName={walletAddress}></Label>
-        <Label center="center" valueName="Quantidade"></Label>
-        <Label valueName={amountBTC}></Label>
+        <div className="wallet">
+        <Label center="center" valueName="Endereço da carteira:"></Label><br></br>
+          <button onClick={() => handleCopyURL(3)} className="copyWallet">
+          <p title='Copiar'> {walletAddress} </p>
+          </button>
+        </div>
+        <div className="amount">
+          <Label center="center" valueName="Quantidade em BTC"></Label><br></br>
+          <p>$ {amountBTC}</p>
+        </div>
         </>
          }
         
-    </ContentLarge>
     </>
   )
 }
