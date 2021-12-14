f7d6fec5defaa5541382d1eb3bf0fae376da8e54
Jeferson Bitencourt <jeferson267@hotmail.com>
2021-12-14 14:40:02 -0300

    Changes

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 export const api = axios.create({
-   baseURL: 'http://localhost:5000/api/',
-   //baseURL: 'https://helpls.azurewebsites.net/api/',
+   //baseURL: 'http://localhost:5000/api/',
+   baseURL: 'https://helpls.azurewebsites.net/api/',
    validateStatus: (status) => {
       return status >= 200 && status <= 500
    }
